eac71031758a13d8e8b5671102d891288029a63d
whatarelive <adriangutierrezmartinez378@gmail.com>
2024-05-16 09:46:28 -0400

    Configuracion base de TailWind_CSS

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,8 @@ module.exports = function(api) {
   return {
     presets: [
       ['babel-preset-expo'],
-      '@babel/preset-typescript'
-   ],
+      '@babel/preset-typescript',
+    ],
+    plugins: ["nativewind/babel"],
   };
 };
