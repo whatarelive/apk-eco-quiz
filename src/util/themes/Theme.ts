3c81927eb1b317212d620685eabbca75a3aa0755
whatarelive <adriangutierrezmartinez378@gmail.com>
2024-05-16 11:23:34 -0400

    Nueva paleta de colores

--- src/util/themes/Theme.ts
+++ src/util/themes/Theme.ts
@@ -11,3 +11,23 @@ export const fontSize = {
     body: 14,
     subHeading: 16,
 }
+
+const primary = {
+  green_light: "#A4CBBB",
+  green_base: "#16915C",
+  green_dark: "#0D6640",
+  green_veryDark: "#02482A",
+  brown_ligt: "#FFF1E4",
+  brown_base: "#FFE4C4",
+  brown_clay: "#F6A051",
+  brown_lightDark: "#CA803D",
+  brown_dark: "#97746B",
+  brown_veryDark: "#75240E",
+  salmon_light: "#F66A51",
+  salmon_dark: "#BE513D",
+  white: '#FFFFFF',
+  black: '#2A2C2C'
+};
+
+
+export const theme = primary;
